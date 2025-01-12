6cf033c66e7e8af6fea935407d7913c20fad315b
aksharavind <akshayaaravind23@gmail.com>
2025-01-12 21:05:05 +0530

    commited

---
+++ demos/src/main/java/com/example/demos/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.demos.controller;
+
+import com.example.demos.entity.User;
+import com.example.demos.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5175")  // Replace with your frontend's port
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+}
---
+++ demos/src/main/java/com/example/demos/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.demos.repository;
+
+import com.example.demos.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
